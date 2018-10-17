d907a078610c17631dcd81ab421a0d7c3a192a2c
Jubril Oyesiji <jubriloyesiji@Jubrils-iMac.local>
2018-10-16 23:21:20 -0400

    made changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,27 @@
-import {MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule  } from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule  } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule ],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule ],
+  imports: [
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatToolbarModule
+   ],
+  exports: [
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatIconModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatToolbarModule ]
 })
 export class MaterialModule { }
